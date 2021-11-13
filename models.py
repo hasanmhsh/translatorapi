a65866fbb550acb62a80ecb8712fe8c4a98663bb
hasan m.shehata <hasan.mh.sh@gmail.com>
2021-11-13 21:32:34 +0200

    store avatar in db

--- models.py
+++ models.py
@@ -38,7 +38,7 @@ def setup_db(app, database_url=database_url):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    # db.create_all()
 
 
 
