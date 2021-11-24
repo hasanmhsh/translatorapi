40b9ae253569a99e24c524ab7bd12abc49d121bf
Ubuntu <ubuntu@ip-172-31-16-43.us-west-2.compute.internal>
2021-11-24 16:59:00 +0000

    aws_command

--- models.py
+++ models.py
@@ -18,7 +18,7 @@ scheme = os.getenv('DB_SCHEME', "postgres")
 username = os.getenv('DB_USER', "postgres")
 password = os.getenv('DB_PASSWORD', "postgres")
 port = os.getenv('DB_PORT', 5432)
-db_name = os.getenv('DB_NAME', "translatordb2")
+db_name = os.getenv('DB_NAME', "translatordb")
 host = os.getenv('DB_HOST', 'localhost')
 
 database_url = os.getenv("DATABASE_URL","{}://{}:{}@{}:{}/{}".format(scheme,username,password,host,port,db_name))
