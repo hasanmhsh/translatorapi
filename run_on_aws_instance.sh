335990638da2a1edb3edf2ce0f3e637479171b07
hasanmhsh <hasan.mh.sh@gmail.com>
2022-02-20 19:37:36 +0000

    save repo 20-02-2022

---
+++ run_on_aws_instance.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+# openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365
+# This command writes a new certificate in cert.pem with its corresponding private key in key.pem, with a validity period of 365 days.
+
+nohup ./runwithgunicorn.sh &
