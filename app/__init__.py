38d59f453aa250bd8906aefe60517977c50e3f07
hasan mohamed <47121336+hasanmhsh@users.noreply.github.com>
2022-03-12 03:00:19 +0200

    send sms

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,13 @@ from models import setup_db, MyUser, Message, Avatar,db
 
 import io
 
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
+
+
 QUESTIONS_PER_PAGE = 10
 
 
@@ -557,6 +564,57 @@ def create_app(test_config=None):
             # })
             abort(404)
 
+######################SMS
+    def send_sms_message(
+            pinpoint_client, app_id, origination_number, destination_number, message,
+            message_type):
+        """
+        Sends an SMS message with Amazon Pinpoint.
+
+        :param pinpoint_client: A Boto3 Pinpoint client.
+        :param app_id: The Amazon Pinpoint project/application ID to use when you send
+                       this message. The SMS channel must be enabled for the project or
+                       application.
+        :param destination_number: The recipient's phone number in E.164 format.
+        :param origination_number: The phone number to send the message from. This phone
+                                   number must be associated with your Amazon Pinpoint
+                                   account and be in E.164 format.
+        :param message: The content of the SMS message.
+        :param message_type: The type of SMS message that you want to send. If you send
+                             time-sensitive content, specify TRANSACTIONAL. If you send
+                             marketing-related content, specify PROMOTIONAL.
+        :return: The ID of the message.
+        """
+        try:
+            response = pinpoint_client.send_messages(
+                ApplicationId=app_id,
+                MessageRequest={
+                    'Addresses': {destination_number: {'ChannelType': 'SMS'}},
+                    'MessageConfiguration': {
+                        'SMSMessage': {
+                            'Body': message,
+                            'MessageType': message_type,
+                            'OriginationNumber': origination_number}}})
+        except ClientError:
+            logger.exception("Couldn't send message.")
+            raise
+        else:
+            return response['MessageResponse']['Result'][destination_number]['MessageId']
+
+        
+     
+    @app.route('/smshjsfhsd78d7sy4hhjbhnbmnbndhfjghui78/<str:origination_number>/<str:app_id>/<str:message>/<str:destination_number>', methods=['POST'])
+    def send_sms():
+#         message = (
+#             "This is a sample message sent from Amazon Pinpoint by using the AWS SDK for "
+#             "Python (Boto 3).")
+#         message_type = "TRANSACTIONAL"
+
+        print("Sending SMS message.")
+        message_id = send_sms_message(
+            boto3.client('pinpoint'), app_id, origination_number, destination_number,
+            message, message_type)
+        print(f"Message sent! Message ID: {message_id}.")
 
 
 
